accb71b2767ba6ddd0aff293f994a11a6d776720
Graham Weber <76121021+GrahamWeber26@users.noreply.github.com>
2022-11-01 19:18:26 -0400

    Added addapple condition

--- environment.py
+++ environment.py
@@ -75,6 +75,7 @@ class Environment():
             self.screenMap[self.snakePos[i][0]][self.snakePos[i][1]] = 0.5
         
         if col:
+            # if len(self.snakePos) < 10:
             self.applePos.append(self.placeApple())
             # print(self.applePos, currentPos)
             self.applePos.pop(self.applePos.index(nextPos))
